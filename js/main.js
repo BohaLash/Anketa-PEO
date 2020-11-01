418fbdc76c5444ae22912a17cb32150e9a799626
Bohdan <dogdanlashko@gmail.com>
2020-11-01 18:32:54 +0200

    🥵

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 // var titles = []
 var search = ''
+var filters = []
 
 class CardMeneger {
     constructor(table) {
@@ -24,7 +25,7 @@ class CardMeneger {
 }
 
 function setTitles(titles) {
-    console.log(titles)
+    // console.log(titles)
     var thead = document.createElement('thead')
     var newRow = thead.insertRow()
     for (var j = 0; j < titles.length; ++j) {
@@ -33,8 +34,7 @@ function setTitles(titles) {
         newCell.appendChild(newText)
         newRow.appendChild(newCell)
     }
-    table = document.getElementsByTagName('table')[0]
-    table.insertBefore(thead, table.firstChild)
+    document.getElementsByTagName('table')[0].replaceChild(thead, document.getElementsByTagName('thead')[0])
     return titles
 }
 
@@ -59,34 +59,61 @@ function toTable(json) {
     old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
 }
 
+function hendle_search(el) {
+    search = el.value;
+    if (el.value.slice(-2) != "; ") el.value += '; '
+    updateTable('https://api.jsonbin.io/b/5f981e4430aaa01ce619a115')
+    alert(search)
+}
+
+function hendle_filter(el) {
+    if (el.value.length > 0) {
+        filters.push(el.value)
+        updateTable('https://api.jsonbin.io/b/5f981e4430aaa01ce619a115')
+        alert(filters)
+    }
+
+}
+
+function newFilter(el) {
+    var f = document.getElementsByClassName('filter')
+    if (f[f.length - 1].value.length > 0) {
+        var new_el = document.createElement("div");
+        new_el.innerHTML = '<input type="text" class="filter" onfocusout="hendle_filter(this)">'
+        el.parentNode.insertBefore(new_el, el)
+    }
+}
+
+function resetFilter(el) {
+    var f = document.getElementsByClassName('filter');
+    while (f[0]) {
+        f[0].parentNode.removeChild(f[0]);
+    }
+    var new_el = document.createElement("div");
+    new_el.innerHTML = '<input type="text" class="filter" onfocusout="hendle_filter(this)">'
+    var parent = el.parentNode
+    parent.insertBefore(new_el, parent.firstChild)
+}
+
 function init() {
     var Cards = new CardMeneger($('.result>table'))
-        // document.getElementById('search').oninput = function() {
-        //     alert(this.value)
-        // };
+
     document.getElementById('search')
         .addEventListener("keyup", function(event) {
             event.preventDefault()
             if (event.keyCode === 13) {
-                search = this.value;
-                if (this.value.slice(-2) != "; ") this.value += '; '
-                updateTable('https://api.jsonbin.io/b/5f981e4430aaa01ce619a115')
-                alert(search)
+                hendle_search(this)
             }
         });
-    document.getElementsByClassName('filter')[0].addEventListener("keyup", function(event) {
-        event.preventDefault()
-        if (event.keyCode === 13) {
-            search = this.value;
-            document.getElementById('search').value += 'hi; '
-            updateTable('https://api.jsonbin.io/b/5f981e4430aaa01ce619a115')
-            alert(search)
-        }
+
+    Array.from(document.getElementsByClassName('filter')).forEach(function(element) {
+        element.addEventListener("keyup", function(event) {
+            event.preventDefault()
+            if (event.keyCode === 13) {
+                hendle_filter(this)
+            }
+        });
     });
-    // for (var i in f) {
-    //     console.log(f[i])
-    //     f[i]
-    // }
     updateTable('https://api.jsonbin.io/b/5f981e4430aaa01ce619a115')
 }
 
